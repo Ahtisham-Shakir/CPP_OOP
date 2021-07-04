3df5116c06a8a2b3494b00068a4e85028c7b153c
Ahtisham <ahtishamshakir777@gmail.com>
2021-07-04 00:51:00 -0700

    Specifying Derived Class & syntax

---
+++ Inheritance/Specifying Derived Class/Example_1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class move
+{
+protected:
+    int position;
+
+public:
+    move()
+    {
+        position = 0;
+    }
+    void forward()
+    {
+        position++;
+    }
+    void show()
+    {
+        cout << "Position= " << position << endl;
+    }
+};
+class move2 : public move
+{
+public:
+    void backward()
+    {
+        position--;
+    }
+};
+int main()
+{
+    move2 m;
+    m.show();
+    m.forward();
+    m.show();
+    m.backward();
+    m.show();
+    return 0;
+}
