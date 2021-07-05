af06b7d5b443e573b5f1c251d2002962f7472bee
Ahtisham <ahtishamshakir777@gmail.com>
2021-07-05 06:48:54 -0700

    Public Inheritance

---
+++ Inheritance/Visibility mode of parent class/Example_1.cpp
@@ -0,0 +1,48 @@
+/*  The accessibility of derived class in public inheritance is as follow
+      1. The Derived class can access public members of parent class.
+      2. The Derived class can access protected members of parent class.
+      3. The Derived class cannot access private members of parent class.
+
+    The accessibility of an object of derived class is as follows:
+      1. The object of derived class can access public members of parent class.
+      2. The object of derived class cannot access protected members of parent class.
+      3. The object of derived class cannot access private members of parent class.
+
+*/
+
+#include <iostream>
+using namespace std;
+class parent
+{
+public:
+    int a;
+
+protected:
+    int b;
+
+private:
+    int c;
+};
+class child : public parent
+{
+public:
+    void in()
+    {
+        cout << "Enter a: ";
+        cin >> a;
+        cout << "Enter b: ";
+        cin >> b;
+    }
+    void show()
+    {
+        cout << "a= " << a << endl;
+        cout << "b= " << b << endl;
+    }
+};
+int main()
+{
+    child obj;
+    obj.in();
+    obj.show();
+    return 0;
+}
