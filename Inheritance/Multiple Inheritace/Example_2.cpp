d019df53faf1f49181fee2c9614372e5eb2039cf
Ahtisham <ahtishamshakir777@gmail.com>
2021-07-07 11:42:52 -0700

    Constructor without Parameters in Multilevel inheritace

---
+++ Inheritance/Multiple Inheritace/Example_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    A()
+    {
+        cout << "Constructor of class A..." << endl;
+    }
+};
+class B
+{
+public:
+    B()
+    {
+        cout << "Constructor of class B..." << endl;
+    }
+};
+class C : public A, public B
+{
+public:
+    C() : A(), B()
+    {
+        cout << "Constructor of class C..." << endl;
+    }
+};
+int main()
+{
+    C obj;
+    return 0;
+}
