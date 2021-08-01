50ea63d2b87a79e52b3394bd910e2c68ed429c10
Ahtisham <ahtishamshakir777@gmail.com>
2021-08-01 10:37:04 -0700

    printing sum of three numbers

---
+++ C++ Practice Programs/Example_1.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+  
+    int a,b,c;
+    cout<<"Enter three numbers: ";
+    cin>>a>>b>>c;
+    cout<<"Sum is= "<<a+b+c;
+    return 0;
+}
