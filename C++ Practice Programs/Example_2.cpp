5b041b55484176d26df146d95372aaf5fd469e93
Ahtisham <ahtishamshakir777@gmail.com>
2021-08-01 10:37:40 -0700

    Basic Data Type

---
+++ C++ Practice Programs/Example_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a; long b; char c; float d; double e;
+    cout<<"Enter int, long, char, float, double: ";
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(3) << d << endl;
+    cout << fixed << setprecision(9) << e << endl;
+    return 0;
+}
